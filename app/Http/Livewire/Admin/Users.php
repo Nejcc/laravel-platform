679b4761c0fb3aec0d61ee0b7a45b3792550c858
Nejc <nejc.cotic@gmail.com>
2021-04-27 16:23:02 +0200

    First feature
    - adding user
    - adding admin route
    - adding vendor for pagination

---
+++ app/Http/Livewire/Admin/Users.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Livewire\Admin;
+
+use App\Models\User;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+class Users extends Component
+{
+    use WithPagination;
+
+    public $perPage = 20;
+    public $orderBy = 'asc';
+    public $orderByInput = 'created_at';
+    public $date_format = 'Y-m-d H:i:s';
+    public $search = '';
+
+    public function updated($propertyName)
+    {
+        $this->validateOnly($propertyName, [
+            'search' => 'min:3|string',
+        ]);
+    }
+
+    public function updatingSearch()
+    {
+        $this->gotoPage(1);
+    }
+
+    public function render()
+    {
+        return view('livewire.admin.users', [
+            'users' => User::search($this->search)
+//                ->select('id', 'name', 'email', 'created_at')
+                ->orderBy($this->orderByInput, $this->orderBy)
+                ->paginate($this->perPage)
+        ]);
+    }
+}
---
+++ resources/views/admin/users/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('title', 'users')
+
+@section('content')
+    <livewire:admin.users/>
+@endsection
